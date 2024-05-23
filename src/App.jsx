e0cbf2cb59a1b453513a0f9169cfaeacf72aba43
Anish Bade <badeanish6045@gmail.com>
2024-05-23 10:59:33 +0545

    brand page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import './App.css';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+
 
 export default function App() {
   return (
     <div>
-      
+      <Navigation/>
+      <Hero/>
     </div>
   )
 }
